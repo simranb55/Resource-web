791eea05da4a94ad48a9c838d59d6518e9f34205
simranb55 <simranb@ocadu.ca>
2025-03-14 12:19:49 -0400

    Add files via upload

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,14 @@
+//button scroll
+$(document).ready(function(){
+    $("#scrollbutton").click(function(){
+        $("html, body").animate({
+            scrollTop: $("#presets").offset().top
+        }, 50);
+    });
+});
+
+
+
 $(document).ready(function() {
     
     $(".popup-text").hide();
@@ -12,7 +23,7 @@ $(document).ready(function() {
         //to show whats selected from the filters
         $(".video-item").each(function() {
             let videoID = $(this).data("video");
-            if (selectedFilters.includes(videoID) || selectedFilters.length === 0) {
+            if (selectedFilters.includes(videoID) || selectedFilters.length == 0) {
                 $(this).show();
             } else {
                 $(this).hide();
@@ -22,7 +33,7 @@ $(document).ready(function() {
 
 
 
-    
+    /*
     
     $(".video-item").click(function() {
         let videoSrc = $(this).find("video source").attr("src"); 
@@ -44,14 +55,36 @@ $(document).ready(function() {
         if (fileLink) {
             $("#popup-text").append(`<br><a href="${fileLink}" download="${fileName}">${fileName}</a>`);
         }
-    });
+    });  */
 
 
+// THIS VERSION WORKS FOR THR <BR> TAGS - .text is replaced with .html
+
+    $(".video-item").click(function() {
+        let videoSrc = $(this).find("video source").attr("src"); 
+        let textForVideo = $(this).find(".popup-text").html(); // the text from the <p> in the textbox
+        let fileLink = $(this).data("link"); 
+
+    
+        let fileName = fileLink ? fileLink.split("/").pop() : ""; // this is so the file name is not "download"
+
+        
+        $(this).find(".popup-text").hide();
+
+        $("#popup").fadeIn();
+        $("#popup-video").attr("src", videoSrc)[0].play();
+        
+        $("#popup-text").html(textForVideo); 
 
 
+        // this is so the file has its original name not "download"
+        if (fileLink) {
+            $("#popup-text").append(`<br><a href="${fileLink}" download="${fileName}">${fileName}</a>`);
+        }
+    });  
 
 
-    // closing the pop up
+    // closing the pop up (outside the screen)
     $("#popup").click(function(event) {
         if ($(event.target).closest(".popup-content").length === 0) {
             $(this).fadeOut();
@@ -59,12 +92,10 @@ $(document).ready(function() {
         }
     });
 
-
-    //text shows up when i click the close button
+    //text shows up when i click the close BUTTON
 
     $("#close-popup").click(function() {
         $("#popup").fadeOut();
-        $("#popup-video").attr("src", ""); // looping the videos
         $(".video-item").each(function() {
         });
         
@@ -73,3 +104,39 @@ $(document).ready(function() {
 
 
 //???  $("#popup-video")[0].pause();
+
+
+//TUTORIAL PAGE FONCTIONS
+
+//slider function
+
+$(document).ready(function() {
+    let currentIndex = 0;
+
+    
+    function updateSlider() {
+        $(".slider-item").hide(); 
+        $(".slider-item").eq(currentIndex).show(); 
+
+        $(".steps").removeClass("show");
+        $(".steps").eq(currentIndex).addClass("show");
+    }
+
+    // nect button (right)
+    $("#next").click(function() {
+        currentIndex = (currentIndex + 1) % $(".slider-item").length; 
+        updateSlider();
+    });
+
+    // back button (left)
+    $("#prev").click(function() {
+        currentIndex = (currentIndex - 1 + $(".slider-item").length) % $(".slider-item").length; 
+        updateSlider();
+    });
+
+    // this makes the slider work?
+    updateSlider();
+});
+
+
+
